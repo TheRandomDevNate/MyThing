87a46b3d60f6eae7038ac995d4c41180b95ca3a6
TheRandomDevNate <115485803+TheRandomDevNate@users.noreply.github.com>
2022-11-22 19:23:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<html>
+	<head>
+		<title>Hello</title>
+	</head>
+	<body style="background-color: black;">
+		<p style="text-align: center; flex-direction: column; color: white;">hello</p>
+	</body>
+  <?php
+  echo "Hello, World!";
+  ?>
+</html>
